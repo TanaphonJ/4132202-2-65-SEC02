c5aa8da62ad2bf1ccb1b3990be5787187e91a024
TanaphonJ <640112418056@bru.ac.th>
2022-12-08 12:15:58 +0700

    LAB4

---
+++ LAB4/php/model/condb.php
@@ -0,0 +1,12 @@
+<?php
+$host = "db";   //db service from dock-compose
+$user = "root";
+$pass = "1234";
+$db = "db_student";
+
+try{
+    $link = mysqli_connect($host,$user,$pass,$db);
+    mysqli_query($link, "SET NAMES utf8");
+} catch (Exception $e) {
+    echo "An error is" . $e . " :: " . mysqli_error($link);
+}
